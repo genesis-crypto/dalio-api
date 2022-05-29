86a9e385881e775956a20d975be257522db29be2
Pedro Cardozo <p-cardozo@hotmail.com>
2022-05-29 16:38:01 -0300

    feat: adding end-point filter principle

--- src/controller/categories/getCategories.ts
+++ src/controller/categories/getCategories.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { getAll, getFromUser } from "../../model/categories";
+import { getFromCategory } from "../../model/subcategories";
 
 const getAllCategories = async (req: Request, res: Response) => {
     try {
@@ -19,7 +20,45 @@ const getUserCategories = async (req: Request, res: Response) => {
             throw new Error("missing arguments");
         }
 
-        const categoriesFromUser = await getFromUser(id);
+        const categoriesFromUser = (await getFromUser({ id })) as any[];
+
+        if (categoriesFromUser?.length !== 0) {
+            const arrayIdCategories: number[] = categoriesFromUser.map(
+                (item: { id: any }) => item.id
+            );
+
+            const subcategories = (await getFromCategory({
+                ids: arrayIdCategories,
+            })) as any[];
+
+            const response = categoriesFromUser.map((category) => {
+                const responseItem: Partial<{
+                    id: number;
+                    category: string;
+                    category_description: string;
+                    subcategory: {
+                        subcategory: string;
+                        subcategory_description: string;
+                    }[];
+                }> = {};
+                responseItem.id = category.id;
+                responseItem.category = category.nome;
+                responseItem.category_description = category.description;
+
+                const filterSubcategory = subcategories
+                    ?.filter((item) => item.id_category == category.id)
+                    ?.map((sub) => ({
+                        subcategory: sub.nome,
+                        subcategory_description: sub.description,
+                    }));
+
+                responseItem.subcategory = filterSubcategory;
+
+                return responseItem;
+            });
+
+            return res.status(200).send({ code: 200, data: response });
+        }
 
         return res.status(200).send({ code: 200, data: categoriesFromUser });
     } catch (err) {
--- src/model/subcategories/index.ts
+++ src/model/subcategories/index.ts
@@ -2,21 +2,21 @@ import db from "../../config/database";
 
 const getAll = async () => {
     try {
-        return await db("category").select("*").limit(40);
+        return await db("subcategory").select("*").limit(40);
     } catch (err) {
         return new Error("something get wrong");
     }
 };
 
-const getFromUser = async ({ id }: { id: number }) => {
+const getFromCategory = async ({ ids }: { ids: number[] }) => {
     try {
-        return await db("category")
+        return await db("subcategory")
             .select("*")
-            .where("id_user", "=", id)
+            .whereIn("id_category", ids)
             .limit(40);
     } catch (err) {
         return new Error("something get wrong");
     }
 };
 
-export { getAll, getFromUser };
+export { getAll, getFromCategory };
